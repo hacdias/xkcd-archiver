89b2517ffc44bdf0e8d8d93d72564c25d0746866
Henrique Dias <hacdias@gmail.com>
2019-04-27 09:56:53 +0100

    feat: xkcd cloner
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ bin/index.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra')
+const yargs = require('yargs')
+const { basename, join } = require('path')
+const { getLatestId, getComic } = require('../lib/xkcd')
+const { homePage, comicPage } = require('../lib/html')
+const { pad, progress } = require('../lib/helpers')
+
+const argv = yargs
+  .usage('$0', 'Clones XKCD comics. By default it only downloads the missing comics.')
+  .scriptName('xkcd-clone')
+  .option('dir', {
+    alias: 'd',
+    describe: 'Output directory',
+    type: 'string',
+    demandOption: true
+  }).option('empty', {
+    alias: 'e',
+    describe: 'Redownload all comics',
+    type: 'boolean'
+  })
+  .help()
+  .argv
+
+async function write ({ data, img }, dir) {
+  try {
+    await fs.outputJSON(join(dir, 'info.json'), data, { spaces: '\t' })
+    const dest = fs.createWriteStream(join(dir, basename(data.img)))
+    img.body.pipe(dest)
+    await fs.outputFile(join(dir, 'index.html'), comicPage(data))
+  } catch (err) {
+    await fs.remove(dir)
+    throw err
+  }
+}
+
+async function run () {
+  console.log(`😊 Going to clone XKCD to ${argv.dir}`)
+
+  let added = []
+  let errored = []
+
+  try {
+    console.log(`🔍 Finding the latest comic`)
+    const latest = await getLatestId()
+    console.log(`😁 Found! We're on comic number ${latest}!`)
+
+    await fs.ensureDir(argv.dir)
+    if (argv.empty) {
+      await fs.emptyDir(argv.dir)
+    }
+
+    for (let i = 1; i <= latest; i++) {
+      const num = pad(i, 4)
+      const dir = join(argv.dir, num)
+
+      if (await fs.pathExists(dir)) {
+        const data = await fs.readJSON(join(dir, 'info.json'))
+        added.push({ id: i, title: data.title, num })
+        await fs.outputFile(join(dir, 'index.html'), comicPage(data))
+        continue
+      } else if (i === 404) {
+        progress(`📦 404 not found 😵`)
+        continue
+      } else {
+        progress(`📦 Fetching ${i} out of ${latest}`)
+      }
+
+      let comic = null
+
+      try {
+        comic = await getComic(i)
+      } catch (err) {
+        progress(`😢 Could not fetch ${i}, will try again later\n`)
+        errored.push(i)
+      }
+
+      await write(comic, dir)
+      added.push({
+        id: i,
+        title: comic.data.title,
+        num: num
+      })
+    }
+  } catch (err) {
+    console.log(`🐉 ${err.stack}`)
+    process.exit(1)
+  }
+
+  // TODO: redownload errored
+
+  if (errored.length === 0) {
+    progress(`📦 All comics fetched\n`)
+  } else {
+    progress(`📦 Some comics fetched\n`)
+  }
+
+  added = added.sort((a, b) => a.num - b.num)
+  await fs.copyFile(join(__dirname, '../node_modules/tachyons/css/tachyons.min.css'), join(argv.dir, 'tachyons.css'))
+  await fs.outputFile(join(argv.dir, 'index.html'), homePage(added))
+}
+/*
+
+const clone = async ({ baseDir, empty, onlyMissing }) => {
+  /*
+
+  for (const num of errored) {
+    for (let i = 0; i < 3; i++) {
+      try {
+        const comic = await getComic(i)
+        await write(comic, dir)
+        break
+      } catch (err) {
+        if (i === 2) {
+          console.log(`😢 ${num} could not be fetched: ${err.toString()}`)
+        }
+      }
+    }
+  }
+
+}
+ */
+
+run()
